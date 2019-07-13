5ad2f35db0527ce1c48861f9cdfb7add58057be0
Ian Smith <isilversmith810@gmail.com>
2019-07-13 13:13:14 -0400

    Fixed /claimblocks

--- src/com/kicas/rp/command/CommandClaimBlocks.java
+++ src/com/kicas/rp/command/CommandClaimBlocks.java
@@ -1,19 +1,17 @@
 package com.kicas.rp.command;
 
 import com.kicas.rp.RegionProtection;
-import com.kicas.rp.data.Region;
+import com.kicas.rp.data.DataManager;
 import com.kicas.rp.util.TextUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import static org.bukkit.Bukkit.getPlayer;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 /**
  * Allows server operators to view and adjust the claim blocks for a given player.
@@ -24,22 +22,10 @@ public class CommandClaimBlocks extends TabCompleterBase implements CommandExecu
     @Override
     public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
         // Assume /claimblocks <self> view
-        if (args.length < 1) {
-            if (!(sender instanceof Player))
-                return false;
-            sender.sendMessage(ChatColor.GOLD + sender.getName() + " has " + RegionProtection.getDataManager()
-                    .getClaimBlocks(((Player) sender).getUniqueId()) + " claim blocks.");
-            int remaining = RegionProtection.getDataManager().getClaimBlocks(((Player) sender).getUniqueId()), used = 0;
-            for (Region claim : RegionProtection.getDataManager().getRegionsInWorld(((Player)sender).getWorld())
-                    .stream().filter(region -> !region.isAdminOwned() &&
-                            region.isOwner(((Player) sender).getUniqueId())).collect(Collectors.toList()))
-                used += claim.area();
-            TextUtils.sendFormatted(sender, "&(gold)Total Blocks: %0\nUsed: %1\nAvailable: %2",
-                    used + remaining, used, remaining);
-            return true;
-        }
-    
-        Player player = getPlayer(args[0]);
+        if (args.length == 0)
+            return false;
+
+        UUID player = DataManager.uuidForUsername(args[0]);
         if(player == null) {
             sender.sendMessage(ChatColor.RED + "Invalid username: " + args[0]);
             return true;
@@ -47,15 +33,8 @@ public class CommandClaimBlocks extends TabCompleterBase implements CommandExecu
     
         // Assume /claimblocks <player> view
         if (args.length < 2) {
-            sender.sendMessage(ChatColor.GOLD + player.getName() + " has " + RegionProtection.getDataManager()
-                    .getClaimBlocks(player.getUniqueId()) + " claim blocks.");
-            int remaining = RegionProtection.getDataManager().getClaimBlocks(((Player) sender).getUniqueId()), used = 0;
-            for (Region claim : RegionProtection.getDataManager().getRegionsInWorld(((Player)sender).getWorld())
-                    .stream().filter(region -> !region.isAdminOwned() &&
-                            region.isOwner(((Player) sender).getUniqueId())).collect(Collectors.toList()))
-                used += claim.area();
-            TextUtils.sendFormatted(sender, "&(gold)Total Blocks: %0\nUsed: %1\nAvailable: %2",
-                    used + remaining, used, remaining);
+            TextUtils.sendFormatted(sender, "&(gold)%0 has {&(aqua)%1} claim blocks.", args[0],
+                    RegionProtection.getDataManager().getClaimBlocks(player));
             return true;
         }
 
@@ -66,17 +45,10 @@ public class CommandClaimBlocks extends TabCompleterBase implements CommandExecu
         }
 
         // View the number of claim blocks they have
-        if("view".equalsIgnoreCase(args[1])) {
-            sender.sendMessage(ChatColor.GOLD + player.getName() + " has " + RegionProtection.getDataManager()
-                    .getClaimBlocks(player.getUniqueId()) + " claim blocks.");
-            int remaining = RegionProtection.getDataManager().getClaimBlocks(((Player) sender).getUniqueId()), used = 0;
-            for (Region claim : RegionProtection.getDataManager().getRegionsInWorld(((Player)sender).getWorld())
-                    .stream().filter(region -> !region.isAdminOwned() &&
-                            region.isOwner(((Player) sender).getUniqueId())).collect(Collectors.toList()))
-                used += claim.area();
-            TextUtils.sendFormatted(sender, "&(gold)Total Blocks: %0\nUsed: %1\nAvailable: %2",
-                    used + remaining, used, remaining);
-        }else{
+        if("view".equalsIgnoreCase(args[1]))
+            TextUtils.sendFormatted(sender, "&(gold)%0 has {&(aqua)%1} claim blocks.", args[0],
+                    RegionProtection.getDataManager().getClaimBlocks(player));
+        else{
             // Secondary args check
             if(args.length < 3) {
                 sender.sendMessage(ChatColor.RED + "Usage: /claimblocks <name> <add|remove> <amount>");
@@ -96,17 +68,17 @@ public class CommandClaimBlocks extends TabCompleterBase implements CommandExecu
             }
 
             // Check to make sure we're not going to end up with negative claim blocks
-            if(removing && amount > RegionProtection.getDataManager().getClaimBlocks(player.getUniqueId())) {
+            if(removing && amount > RegionProtection.getDataManager().getClaimBlocks(player)) {
                 sender.sendMessage(ChatColor.RED + "You cannot take that many claim blocks from this player " +
                         "otherwise they would have negative claim blocks.");
                 return true;
             }
 
             // Modify the claim blocks and notify the sender
-            RegionProtection.getDataManager().modifyClaimBlocks(player.getUniqueId(), (removing ? -1 : 1) * amount);
+            RegionProtection.getDataManager().modifyClaimBlocks(player, (removing ? -1 : 1) * amount);
             sender.sendMessage(ChatColor.GOLD + (removing ? "Removed " : "Added ") + amount + " claim blocks " +
-                    (removing ? "from " : "to ") + player.getName() + ". They now have " +
-                    RegionProtection.getDataManager().getClaimBlocks(player.getUniqueId()) + " claim blocks.");
+                    (removing ? "from " : "to ") + args[0] + ". They now have " +
+                    RegionProtection.getDataManager().getClaimBlocks(player) + " claim blocks.");
         }
 
         return true;
